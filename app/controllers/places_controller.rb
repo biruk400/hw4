f3101ee103bce0ff12536f1b584c0c040e69bec9
biruk400 <biruk.bekele@kellogg.northwestern.edu>
2022-06-08 09:04:04 +0000

    update2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,6 +2,7 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.all
+    @myplaces = Place.where({"user_id" => session["user_id"]})
   end
 
   def show
@@ -14,10 +15,14 @@ class PlacesController < ApplicationController
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["place"]["name"]
-    @place.save
+    if @current_user
+      @place = Place.new
+      @place["name"] = params["place"]["name"]
+      @place["user_id"] = @current_user["id"]
+      @place.save
+    else
+      flash["notice"] = "Login first."
+    end
     redirect_to "/places"
   end
-
 end
